2e5abf3e3182cd1b223148e7fa30c9076dc350cc
liyuxiao222 <89355060+liyuxiao2@users.noreply.github.com>
2024-08-05 15:26:17 -0400

    intialized react project

---
+++ leaderboard/src/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [    
+  './src/**/*.{html,js,jsx,ts,tsx}',  // Paths to your template files
+  './public/index.html', ],              // Path to your main HTML file (if applicable)
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
